2a86a1ee010c212f0298c4f5b263dbf4eda9af33
Pepe Iborra <pepeiborra@gmail.com>
2019-12-26 09:17:21 +0000

    #46 Code action to add suggested imports

--- src/Development/IDE/Types/Location.hs
+++ src/Development/IDE/Types/Location.hs
@@ -21,14 +21,19 @@ module Development.IDE.Types.Location
     , filePathToUri
     , filePathToUri'
     , uriToFilePath'
+    , rangeFromSrcSpan
+    , readSrcSpan
     ) where
 
+import Control.Applicative
 import Language.Haskell.LSP.Types (Location(..), Range(..), Position(..))
 import Control.DeepSeq
+import Control.Monad
 import Data.Binary
 import Data.Maybe as Maybe
 import Data.Hashable
 import Data.String
+import FastString
 import System.FilePath
 import qualified Language.Haskell.LSP.Types as LSP
 import Language.Haskell.LSP.Types as LSP (
@@ -38,6 +43,8 @@ import Language.Haskell.LSP.Types as LSP (
   , toNormalizedUri
   , fromNormalizedUri
   )
+import GHC
+import Text.ParserCombinators.ReadP as ReadP
 
 
 -- | Newtype wrapper around FilePath that always has normalized slashes.
@@ -79,6 +86,48 @@ noFilePath = "<unknown>"
 noRange :: Range
 noRange =  Range (Position 0 0) (Position 100000 0)
 
+rangeFromSrcSpan :: SrcSpan -> Range
+rangeFromSrcSpan ss = Range (positionFromSrcLoc $ srcSpanStart ss) (positionFromSrcLoc $ srcSpanEnd ss)
+  where
+    positionFromSrcLoc (RealSrcLoc sl)= Position (srcLocLine sl - 1) (srcLocCol sl - 1)
+    positionFromSrcLoc (UnhelpfulLoc err) = error (show err)
 
 showPosition :: Position -> String
 showPosition Position{..} = show (_line + 1) ++ ":" ++ show (_character + 1)
+
+-- | Parser for the GHC output format
+readSrcSpan :: ReadS SrcSpan
+readSrcSpan = readP_to_S (singleLineSrcSpanP <|> multiLineSrcSpanP)
+  where
+    singleLineSrcSpanP, multiLineSrcSpanP :: ReadP SrcSpan
+    singleLineSrcSpanP = do
+      fp <- filePathP
+      l  <- readS_to_P reads <* char ':'
+      c0 <- readS_to_P reads
+      c1 <- (char '-' *> readS_to_P reads) <|> pure c0
+      let from = mkSrcLoc fp l c0
+          to   = mkSrcLoc fp l c1
+      return $ mkSrcSpan from to
+
+    multiLineSrcSpanP = do
+      fp <- filePathP
+      s <- parensP (srcLocP fp)
+      void $ char '-'
+      e <- parensP (srcLocP fp)
+      return $ mkSrcSpan s e
+
+    parensP :: ReadP a -> ReadP a
+    parensP = between (char '(') (char ')')
+
+    filePathP :: ReadP FastString
+    filePathP = fromString <$> (readFilePath <* char ':') <|> pure ""
+
+    srcLocP :: FastString -> ReadP SrcLoc
+    srcLocP fp = do
+      l <- readS_to_P reads
+      void $ char ','
+      c <- readS_to_P reads
+      return $ mkSrcLoc fp l c
+
+    readFilePath :: ReadP FilePath
+    readFilePath = some ReadP.get
