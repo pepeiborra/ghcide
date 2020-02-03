dec722d2192737d72e74a4858cc4cadba2b1c0c6
Pepe Iborra <pepeiborra@gmail.com>
2020-02-03 19:35:03 +0000

    Generate .hi and .hie files during type checking

--- src/Development/IDE/GHC/Compat.hs
+++ src/Development/IDE/GHC/Compat.hs
@@ -33,15 +33,16 @@ module Development.IDE.GHC.Compat(
     supportsHieFiles,
     ) where
 
+import Data.ByteString (ByteString)
 import StringBuffer
 import DynFlags
 import FieldLabel
 
-import qualified GHC
 import GHC hiding (ClassOpSig, DerivD, ForD, IEThingWith, InstD, TyClD, ValD, ModLocation)
+import qualified GHC
 
 #if MIN_GHC_API_VERSION(8,8,0)
-import HieAst
+import Development.IDE.GHC.HieAst
 import HieBin
 import HieTypes
 
@@ -64,8 +65,8 @@ hPutStringBuffer hdl (StringBuffer buf len cur)
              hPutBuf hdl ptr len
 #endif
 
-mkHieFile :: ModSummary -> TcGblEnv -> RenamedSource -> Hsc HieFile
-mkHieFile _ _ _ = return (HieFile () [])
+mkHieFile :: ModSummary -> TcGblEnv -> RenamedSource -> ByteString -> Hsc HieFile
+mkHieFile _ _ _ _ = return (HieFile () [])
 
 writeHieFile :: FilePath -> HieFile -> IO ()
 writeHieFile _ _ = return ()
