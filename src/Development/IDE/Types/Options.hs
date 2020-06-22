ee86c773fba66c7ed13cb83b3e062d8ffa3d1840
Pepe Iborra <pepeiborra@gmail.com>
2020-06-22 12:56:21 +0100

    Add comment on sessionVersion field

--- src/Development/IDE/Types/Options.hs
+++ src/Development/IDE/Types/Options.hs
@@ -31,6 +31,7 @@ data IdeGhcSession = IdeGhcSession
   { loadSessionFun :: FilePath -> IO (IdeResult HscEnvEq, HashSet FilePath)
   -- ^ Returns the Ghc session and the cradle dependencies
   , sessionVersion :: !Int
+  -- ^ Used as Shake key, versions must be unique and not reused
   }
 
 instance Show IdeGhcSession where show _ = "IdeGhcSession"
